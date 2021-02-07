bff7740b70cf6a248652bc4f9f28567e85fc719a
DBNBESPALOV <bdn.bespalov@gmail.com>
2021-02-07 20:50:10 +0300

    рефакторинг

--- src/main/java/my/org/client/execution/ManiExecList.java
+++ src/main/java/my/org/client/execution/ManiExecList.java
@@ -1,5 +1,13 @@
 package my.org.client.execution;
 
-public class ManiExecList {
+import java.util.ArrayList;
 
-}
+public class ManiExecList {
+    public ArrayList<Exec> execution;
+
+    public void addExec(Exec exec){
+        ArrayList<Exec> arrayList = new ArrayList<>();
+        arrayList.add(exec);
+        this.execution = arrayList;
+
+    }}
